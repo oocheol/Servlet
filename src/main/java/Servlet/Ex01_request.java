a2a76b663a7d3f7b94c38cc723a546b9d59b6f95
oocheol <oocheol@naver.com>
2022-04-08 16:16:36 +0900

    22/04/08

--- src/main/java/Servlet/Ex01_request.java
+++ src/main/java/Servlet/Ex01_request.java
@@ -9,7 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 
 // URL Mapping : Servlet 별명
 @WebServlet("/Ex01_request")
+							// HttpServlet 클래스를 상속받음
 public class Ex01_request extends HttpServlet {
+					// service 메소드 : Java의 main 메소드와 같은 역할
+					// 매개변수 : request, response
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
