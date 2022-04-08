386aaf72c2f4fbab7cc65c0274d70da3e24e4a3b
oocheol <oocheol@naver.com>
2022-04-08 16:20:48 +0900

    22/04/08

--- src/main/java/Servlet/Ex01_request.java
+++ src/main/java/Servlet/Ex01_request.java
@@ -12,8 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 							// HttpServlet 클래스를 상속받음
 public class Ex01_request extends HttpServlet {
 					// service 메소드 : Java의 main 메소드와 같은 역할
-					// 매개변수 : request, response
+					// 매개변수 : request(요청), response(응답)
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 1. request 객체 : 사용자가 요청을 했을 때 요청에 대한 모든 정보를 담고 있는 객체
+		//				ex) 요청이 들어온 주소, 사용자가 입력한 정보(id, pw) 등..
+		
+		// 실습1. 요청이 들어온 ip주소 확인하기
+		request.getRemoteHost();
+		
 		
 	}
 
