04f73159599454bfe6f6ae42911eb2c107cdc21d
oocheol <oocheol@naver.com>
2022-04-08 16:14:57 +0900

    init

---
+++ src/main/java/Servlet/Ex01_request.java
@@ -0,0 +1,17 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// URL Mapping : Servlet º°¸í
+@WebServlet("/Ex01_request")
+public class Ex01_request extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
